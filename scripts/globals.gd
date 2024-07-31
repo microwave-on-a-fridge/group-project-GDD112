d835bdfc379b135ccc5b631eb056574ee00fc0ad
pastelle <pastel-x64@proton.me>
2024-08-01 09:03:10 +1200

    Thursday

---
+++ scripts/globals.gd
@@ -0,0 +1,7 @@
+extends Node
+
+var player_position: Vector2
+var enemies_killed: int = 0
+var dead: bool = false
+var score: int = 0
+var grounded: bool = true
---
+++ scripts/player.gd
@@ -0,0 +1,35 @@
+extends CharacterBody2D
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -600.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func _physics_process(delta):
+	
+	if is_on_floor():
+		Globals.grounded = true
+		print("currently grounded")
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("left", "right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
+
+func _on_area_2d_body_entered(_body):
+	print("dead")
+	queue_free()
+	pass # Replace with function body.
